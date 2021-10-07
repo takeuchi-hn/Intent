cd27632aa598fd22129959efbbc9724575f1e8f9
takeuchi-hn <sinnichi406t.t@gmail.com>
2021-10-07 12:21:32 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/taichi/takeuchi/intent/MainActivity.kt
@@ -0,0 +1,22 @@
+package jp.techacademy.taichi.takeuchi.intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent //追記した
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/jp/techacademy/taichi/takeuchi/intent/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package jp.techacademy.taichi.takeuchi.intent.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
