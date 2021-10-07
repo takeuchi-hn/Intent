eb9802470c1bb608956c4d20111fca17dba96796
takeuchi-hn <sinnichi406t.t@gmail.com>
2021-10-07 16:55:46 +0900

    4-5.1　完了

--- app/src/main/java/jp/techacademy/taichi/takeuchi/intent/ui/login/LoggedInUserView.kt
+++
@@ -1,9 +0,0 @@
-package jp.techacademy.taichi.takeuchi.intent.ui.login
-
-/**
- * User details post authentication that is exposed to the UI
- */
-data class LoggedInUserView(
-    val displayName: String
-    //... other data fields that may be accessible to the UI
-)
